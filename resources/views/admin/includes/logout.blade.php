26733dc2d7324027813bfb09a20c51e827177456
jalalkhan1011 <jalal.khan981@gmail.com>
2021-06-25 18:08:03 +0600

    add logout functoin on logout file

--- resources/views/admin/includes/logout.blade.php
+++ resources/views/admin/includes/logout.blade.php
@@ -11,8 +11,17 @@
             <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
             <div class="modal-footer">
                 <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                <a class="btn btn-primary" href="login.html">Logout</a>
+                    <a href="{{ route('logout') }}"
+                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                        <button class="btn btn-danger" type="button">{{ __('Logout') }}</button>
+                    </a>
+
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                        @csrf
+                    </form>
             </div>
         </div>
     </div>
 </div>
+
